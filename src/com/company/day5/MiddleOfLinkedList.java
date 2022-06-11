fea73f87721e5a4499a6aeab18fedad6f1bcd66e
Dipak Kumar <dipak_kumar@intuit.com>
2022-06-12 00:05:09 +0530

    Adding Find Middle Node of Linked List problem

---
+++ src/com/company/day5/MiddleOfLinkedList.java
@@ -0,0 +1,24 @@
+package com.company.day5;
+
+import com.company.utility.ListNode;
+
+public class MiddleOfLinkedList {
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+        listNode.next.next.next = new ListNode(4);
+        listNode.next.next.next.next = new ListNode(5);
+        System.out.println(findMiddleElement(listNode));
+    }
+
+    private static int findMiddleElement(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow.val;
+    }
+}
